a1accf42373474b5d1120aa36e61ec8ca4ec3ae4
CraftonComputerScienceClub <134252327+CraftonComputerScienceClub@users.noreply.github.com>
2024-04-12 19:05:23 -0700

    Add files via upload
    Added project item examples

---
+++ projects.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Projects Page</title>
+    <link href="style.css" rel="stylesheet"/>
+</head>
+<body>
+    <?php
+        include "navbar.html";
+    ?>
+    <ul class="project_list">
+        <li>
+            <h1><strong>Frogger In Python<strong></h1>
+            <a href="#FroggerProject">
+                <img src="banner.png">
+            </a>
+            <hr>
+            <p>Frogger is a frog. frogs are pretty cool. ever seen a frog? me nether. but I still think they are cool even if I haven't seen them. I haven't seen santa and yet I know he is cool.</p>
+        </li>
+        <li>
+            <h1><strong>Frogger In Python<strong></h1>
+            <a href="#FroggerProject">
+                <img src="banner.png">
+            </a>
+            <hr>
+            <p>Frogger is a frog. frogs are pretty cool. ever seen a frog? me nether. but I still think they are cool even if I haven't seen them. I haven't seen santa and yet I know he is cool.</p>
+        </li>
+    </ul>
+
+
+</body>
+</html>
