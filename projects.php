751b875e3267fbfb7c7c9b485e93fd9293e69a2e
Ryan <agn0sth3s1a@gmail.com>
2024-04-12 19:55:45 -0700

    Added images and description to project page

--- projects.php
+++ projects.php
@@ -13,18 +13,18 @@
         <li>
             <h1><strong>Frogger In Python<strong></h1>
             <a href="#FroggerProject">
-                <img src="banner.png">
+                <img src="https://cdn.discordapp.com/attachments/1206802139259736165/1224548404256837752/image.png?ex=66271f12&is=6614aa12&hm=a44e848c821e393512cd10b7ebaaba0388488cbc17e86893034b7d1ec4101031&" />
             </a>
             <hr>
             <p>Frogger is a frog. frogs are pretty cool. ever seen a frog? me nether. but I still think they are cool even if I haven't seen them. I haven't seen santa and yet I know he is cool.</p>
         </li>
         <li>
-            <h1><strong>Frogger In Python<strong></h1>
-            <a href="#FroggerProject">
-                <img src="banner.png">
+            <h1><strong>This website!<strong></h1>
+            <a href="https://github.com/CraftonComputerScienceClub/CSCWebsite">
+                <img src="https://cdn.discordapp.com/attachments/786093818881638431/1228538441617379408/Website_image_1.png?ex=662c6895&is=6619f395&hm=0cf299f5b7ae4814d09c55e58e5dcc6645feb17924a5801692b0575b0b5a256f&">
             </a>
             <hr>
-            <p>Frogger is a frog. frogs are pretty cool. ever seen a frog? me nether. but I still think they are cool even if I haven't seen them. I haven't seen santa and yet I know he is cool.</p>
+            <p>This was our first large-scale group project! It was quite the learning experience, as it had us learn to use both html and php.</p>
         </li>
     </ul>
 
