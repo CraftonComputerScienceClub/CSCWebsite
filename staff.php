f6d95e155b054470af4d7e86f902cb6381383ecb
Blake Benfield <Blakefbenfield101@gmail.com>
2024-04-26 19:29:49 -0700

    Finished Staff Page.

---
+++ staff.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link href="style.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Genos" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>CSC Website</title>
+</head>
+<body>
+<!-- Navigation bar. -->
+<?php
+include "navbar.html";
+?>
+<h1 class="staff-title">
+    Our Staff
+</h1>
+
+<hr/>      <!-- hr makes a line across the screen -->
+
+<div class="staff-cards">
+    <div class="staff-card">
+        <img src="Jakob.png">
+        <p>
+            <span style="font-size: 90px">Jakob Barringer</span>
+            <br>President<br></p>
+    </div>
+    <div class="staff-card">
+        <img src="max.png">
+        <p>
+            <span style="font-size: 90px">Maxwell Smith</span>
+            <br>Vice President<br></p>
+    </div>
+    <div class="staff-card">
+        <img src="blake.png">
+        <p>
+            <span style="font-size: 90px">Blake Benfield</span>
+            <br>Secretary<br></p>
+    </div>
+    <div class="staff-card">
+        <img src="tommy.png">
+        <p>
+            <span style="font-size: 90px">Thomas Van Esch</span>
+            <br>Treasurer<br></p>
+    </div>
+
+    <p></p>
+</div>
+
+</body>
+</html>
+
