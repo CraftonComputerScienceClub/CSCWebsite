bfa30d4338fa7dc66eb1b64ff4dacbf9a8562c12
Ryan <agn0sth3s1a@gmail.com>
2024-04-26 19:04:00 -0700

    Changed the source of the images

--- projects.php
+++ projects.php
@@ -13,7 +13,7 @@
         <li>
             <h1><strong>Frogger In Python<strong></h1>
             <a href="#FroggerProject">
-                <img src="https://cdn.discordapp.com/attachments/1206802139259736165/1224548404256837752/image.png?ex=66271f12&is=6614aa12&hm=a44e848c821e393512cd10b7ebaaba0388488cbc17e86893034b7d1ec4101031&" />
+                <img src="Frogger Image.png" />
             </a>
             <hr>
             <p>Frogger is a frog. frogs are pretty cool. ever seen a frog? me nether. but I still think they are cool even if I haven't seen them. I haven't seen santa and yet I know he is cool.</p>
@@ -21,7 +21,7 @@
         <li>
             <h1><strong>This website!<strong></h1>
             <a href="https://github.com/CraftonComputerScienceClub/CSCWebsite">
-                <img src="https://cdn.discordapp.com/attachments/786093818881638431/1228538441617379408/Website_image_1.png?ex=662c6895&is=6619f395&hm=0cf299f5b7ae4814d09c55e58e5dcc6645feb17924a5801692b0575b0b5a256f&">
+                <img src="Website_image_1.png">
             </a>
             <hr>
             <p>This was our first large-scale group project! It was quite the learning experience, as it had us learn to use both html and php.</p>
